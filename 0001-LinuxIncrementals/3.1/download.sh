0ed6e1b7bdd188d460e1bb1fee8437f1f172d06f
Tad <tad@spotco.us>
2018-06-25 20:02:34 -0400

    Add incrementals for 3.1

---
+++ 0001-LinuxIncrementals/3.1/download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget https://cdn.kernel.org/pub/linux/kernel/v3.x/incr/patch-3.1.1-2.xz -O - | xz -d > 3.1.0001-0002.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v3.x/incr/patch-3.1.2-3.xz -O - | xz -d > 3.1.0002-0003.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v3.x/incr/patch-3.1.3-4.xz -O - | xz -d > 3.1.0003-0004.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v3.x/incr/patch-3.1.4-5.xz -O - | xz -d > 3.1.0004-0005.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v3.x/incr/patch-3.1.5-6.xz -O - | xz -d > 3.1.0005-0006.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v3.x/incr/patch-3.1.6-7.xz -O - | xz -d > 3.1.0006-0007.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v3.x/incr/patch-3.1.7-8.xz -O - | xz -d > 3.1.0007-0008.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v3.x/incr/patch-3.1.8-9.xz -O - | xz -d > 3.1.0008-0009.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v3.x/incr/patch-3.1.9-10.xz -O - | xz -d > 3.1.0009-0010.patch
