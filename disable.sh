5868c498d79eaa8a27f1d789b58c7b50d5a7c5d8
Tad <tad@spotco.us>
2018-08-23 22:58:08 -0400

    Revert of last commit

---
+++ disable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mv CVE-2016-0819/ANY/0001.patch CVE-2016-0819/ANY/0001.patch.disabled;
+mv CVE-2017-0794/3.10/0001.patch CVE-2017-0794/3.10/0001.patch.disabled;
+mv CVE-2017-16USB/ANY/0007.patch CVE-2017-16USB/ANY/0007.patch.disabled
