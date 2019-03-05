2645166c108f6009d603bfdd3115ddbdedf81a9a
Tad <tad@spotco.us>
2019-03-04 20:27:14 -0500

    Add incrementals for 4.19

---
+++ 0001-LinuxIncrementals/4.19/download.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.1-2.xz -O - | xz -d > 4.19.0001-0002.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.2-3.xz -O - | xz -d > 4.19.0002-0003.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.3-4.xz -O - | xz -d > 4.19.0003-0004.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.4-5.xz -O - | xz -d > 4.19.0004-0005.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.5-6.xz -O - | xz -d > 4.19.0005-0006.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.6-7.xz -O - | xz -d > 4.19.0006-0007.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.7-8.xz -O - | xz -d > 4.19.0007-0008.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.8-9.xz -O - | xz -d > 4.19.0008-0009.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.9-10.xz -O - | xz -d > 4.19.0009-0010.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.10-11.xz -O - | xz -d > 4.19.0010-0011.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.11-12.xz -O - | xz -d > 4.19.0011-0012.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.12-13.xz -O - | xz -d > 4.19.0012-0013.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.13-14.xz -O - | xz -d > 4.19.0013-0014.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.14-15.xz -O - | xz -d > 4.19.0014-0015.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.15-16.xz -O - | xz -d > 4.19.0015-0016.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.16-17.xz -O - | xz -d > 4.19.0016-0017.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.17-18.xz -O - | xz -d > 4.19.0017-0018.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.18-19.xz -O - | xz -d > 4.19.0018-0019.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.19-20.xz -O - | xz -d > 4.19.0019-0020.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.20-21.xz -O - | xz -d > 4.19.0020-0021.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.21-22.xz -O - | xz -d > 4.19.0021-0022.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.22-23.xz -O - | xz -d > 4.19.0022-0023.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.23-24.xz -O - | xz -d > 4.19.0023-0024.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.24-25.xz -O - | xz -d > 4.19.0024-0025.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/incr/patch-4.19.25-26.xz -O - | xz -d > 4.19.0025-0026.patch
