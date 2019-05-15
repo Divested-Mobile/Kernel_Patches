0003e5e57e4d56884a947b5e785e6f36c05b5d5b
Tad <tad@spotco.us>
2019-05-15 17:15:54 -0400

    Build fixes

--- disable.sh
+++ disable.sh
@@ -3,3 +3,4 @@ mv CVE-2016-0819/ANY/0001.patch CVE-2016-0819/ANY/0001.patch.disabled;
 mv CVE-2016-10208/^4.9.8/0002.patch CVE-2016-10208/^4.9.8/0002.patch.disabled;
 mv CVE-2017-0794/3.10/0001.patch CVE-2017-0794/3.10/0001.patch.disabled;
 mv CVE-2017-16USB/ANY/0007.patch CVE-2017-16USB/ANY/0007.patch.disabled
+mv CVE-2018-9513/3.18+/0001.patch CVE-2018-9513/3.18+/0001.patch.disabled;
