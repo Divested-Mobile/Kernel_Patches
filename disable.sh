9a828784009d6f4b10692aa8cc79794f87921e89
Tad <tad@spotco.us>
2019-09-04 01:24:22 -0400

    Fixes

--- disable.sh
+++ disable.sh
@@ -5,3 +5,4 @@ mv CVE-2016-10208/^4.9.8/0002.patch CVE-2016-10208/^4.9.8/0002.patch.disabled;
 mv CVE-2017-0794/3.10/0001.patch CVE-2017-0794/3.10/0001.patch.disabled;
 mv CVE-2017-16USB/ANY/0007.patch CVE-2017-16USB/ANY/0007.patch.disabled
 mv CVE-2018-9513/3.18+/0001.patch CVE-2018-9513/3.18+/0001.patch.disabled;
+mv CVE-2019-2324/3.10/0001.patch CVE-2019-2324/3.10/0001.patch.disabled;
