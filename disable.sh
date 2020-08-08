4fd4de1ec26b13b21d5ca3a36f1fe3a3a1c148db
Tad <tad@spotco.us>
2020-08-08 13:30:41 -0400

    Fix breakage

--- disable.sh
+++ disable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mv CVE-2013-2891/^3.11/0001.patch CVE-2013-2891/^3.11/0001.patch.disabled;
 mv CVE-2016-0819/ANY/0001.patch CVE-2016-0819/ANY/0001.patch.disabled;
-mv CVE-2016-10208/^4.9.8/0002.patch CVE-2016-10208/^4.9.8/0002.patch.disabled;
+mv CVE-2016-10208/^4.9.8/0001.patch CVE-2016-10208/^4.9.8/0001.patch.disabled;
 mv CVE-2017-0794/3.10/0001.patch CVE-2017-0794/3.10/0001.patch.disabled;
 mv CVE-2017-16USB/ANY/0007.patch CVE-2017-16USB/ANY/0007.patch.disabled
 mv CVE-2018-9513/3.18+/0001.patch CVE-2018-9513/3.18+/0001.patch.disabled;
