555e0c05210b3adea767be7fb967758cdc7a961d
Tad <tad@spotco.us>
2020-09-12 11:45:25 -0400

    Add a script for updating CIP.txt

---
+++ CIP.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+java -jar "$DOS_BINARY_PATCHER" scraper "$1" > CIP.tmp.1;
+echo -e "CVE-0000-0000\n#The above line must be the last line" >> CIP.tmp.1;
+java -jar "$DOS_BINARY_PATCHER" sort CIP.tmp.1 > CIP.tmp.2;
+echo -e "CVE-0000-0000\n#The above line must be the last line" >> CIP.tmp.2;
+echo "#Last checked "$(date -uI) > CIP.tmp.3;
+cat CIP.tmp.2 >> CIP.tmp.3;
+#mv CIP.txt CIP.txt.bak;
+mv CIP.tmp.3 CIP.txt;
+rm CIP.tmp.1 CIP.tmp.2;
+echo "Please manually sync CIP.txt with Kernel_CVE_Patch_List.txt";
