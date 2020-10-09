73c380c2b4bea1d9fa7996adaf0c03d05ca0d0aa
Tad <tad@spotco.us>
2020-10-09 14:32:18 -0400

    CVE-2016-1583/3.4/0005.patch

--- disable.sh
+++ disable.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mv CVE-2013-2891/^3.11/0001.patch CVE-2013-2891/^3.11/0001.patch.disabled;
 mv CVE-2016-0819/ANY/0001.patch CVE-2016-0819/ANY/0001.patch.disabled;
+mv CVE-2016-1583/3.4/0005.patch CVE-2016-1583/3.4/0005.patch.disabled;
 mv CVE-2016-10208/^4.9.8/0001.patch CVE-2016-10208/^4.9.8/0001.patch.disabled;
 mv CVE-2017-0794/3.10/0001.patch CVE-2017-0794/3.10/0001.patch.disabled;
 mv CVE-2017-16USB/ANY/0007.patch CVE-2017-16USB/ANY/0007.patch.disabled
