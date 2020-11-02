a3fdcb455b0a69e1a0daec7270f0e6506f04848e
Tad <tad@spotco.us>
2020-11-02 13:51:37 -0500

    Smal updates

--- disable.sh
+++ disable.sh
@@ -7,7 +7,7 @@ mv CVE-2017-0794/3.10/0001.patch CVE-2017-0794/3.10/0001.patch.disabled;
 mv CVE-2017-16USB/ANY/0007.patch CVE-2017-16USB/ANY/0007.patch.disabled
 mv CVE-2018-9513/3.18+/0001.patch CVE-2018-9513/3.18+/0001.patch.disabled;
 mv CVE-2019-2324/3.10/0001.patch CVE-2019-2324/3.10/0001.patch.disabled;
-mv CVE-2020-0305/ANY/0001.patch CVE-2020-0305/ANY/0001.patch.disabled;
+mv CVE-2020-0305/^5.5/0001.patch CVE-2020-0305/^5.5/0001.patch.disabled;
 mv 0008-Graphene-Kernel_Hardening/4.4/0024.patch 0008-Graphene-Kernel_Hardening/4.4/0024.patch.disabled;
 mv 0008-Graphene-Kernel_Hardening/4.9/0040.patch 0008-Graphene-Kernel_Hardening/4.9/0040.patch.disabled;
 mv 0008-Graphene-Kernel_Hardening/4.14/0061.patch 0008-Graphene-Kernel_Hardening/4.14/0061.patch.disabled;
