dcb68207fc434c66304b4e851b4c2c740a5e2123
Tad <tad@spotco.us>
2021-09-22 21:52:11 -0400

    Fix a compile error
    Signed-off-by: Tad <tad@spotco.us>

--- disable.sh
+++ disable.sh
@@ -18,3 +18,4 @@ mv CVE-2015-8940/3.4/0002.patch CVE-2015-8940/3.4/0002.patch.disabled;
 mv CVE-2018-11878/prima/0001.patch CVE-2018-11878/prima/0001.patch.disabled;
 mv CVE-2017-5972/3.4/0003.patch CVE-2017-5972/3.4/0003.patch.disabled;
 mv CVE-2017-5972/ANY/0001.patch CVE-2017-5972/ANY/0001.patch.disabled;
+mv CVE-2019-2214/3.4/0004.patch CVE-2019-2214/3.4/0004.patch.disabled;
