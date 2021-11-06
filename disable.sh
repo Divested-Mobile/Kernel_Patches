d45f3eb0e8dbaa0955160fa5861986342f8c6d84
Tad <tad@spotco.us>
2021-11-06 18:32:51 -0400

    Disable CVE-2021-20317
    Signed-off-by: Tad <tad@spotco.us>

--- disable.sh
+++ disable.sh
@@ -19,6 +19,6 @@ mv CVE-2018-11878/prima/0001.patch CVE-2018-11878/prima/0001.patch.disabled;
 mv CVE-2017-5972/3.4/0003.patch CVE-2017-5972/3.4/0003.patch.disabled;
 mv CVE-2017-5972/ANY/0001.patch CVE-2017-5972/ANY/0001.patch.disabled;
 mv CVE-2019-2214/3.4/0004.patch CVE-2019-2214/3.4/0004.patch.disabled;
-mv CVE-2021-20317/^5.3/0001.patch CVE-2021-20317/^5.3/0001.patch.disabled; #breaks qcom event timer
 mv CVE-2019-19449/^5.9/0001.patch CVE-2019-19449/^5.9/0001.patch.disabled;
 rename patch patch.disabled CVE-2021-42008/*/*.patch; #applies again and again
+rename patch patch.disabled CVE-2021-20317/*/*.patch; #breaks qcom event timer
