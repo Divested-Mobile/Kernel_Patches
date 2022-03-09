b0d8fee8304971462e679639ab48de5ea4dca53b
Tad <tad@spotco.us>
2022-03-09 14:55:27 -0500

    Fixup all versions and convert some AOSP diffs to patches
    Signed-off-by: Tad <tad@spotco.us>

--- disable.sh
+++ disable.sh
@@ -3,7 +3,7 @@
 mv CVE-2013-2891/^3.11/0001.patch CVE-2013-2891/^3.11/0001.patch.disabled;
 mv CVE-2016-0819/ANY/0001.patch CVE-2016-0819/ANY/0001.patch.disabled;
 mv CVE-2016-1583/3.4/0005.patch CVE-2016-1583/3.4/0005.patch.disabled;
-mv CVE-2016-10208/^4.9.8/0001.patch CVE-2016-10208/^4.9.8/0001.patch.disabled;
+mv CVE-2016-10208/^4.9/0001.patch CVE-2016-10208/^4.9/0001.patch.disabled;
 mv CVE-2017-0794/3.10/0001.patch CVE-2017-0794/3.10/0001.patch.disabled;
 mv CVE-2017-16USB/ANY/0007.patch CVE-2017-16USB/ANY/0007.patch.disabled;
 mv CVE-2018-9513/3.18+/0001.patch CVE-2018-9513/3.18+/0001.patch.disabled;
