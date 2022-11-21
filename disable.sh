7715533ef0d4fc37073595c407e171f17653131f
Tad <tad@spotco.us>
2022-11-21 07:53:14 -0500

    Disable CVE-2022-3640
    This needs special handling
    
    Signed-off-by: Tad <tad@spotco.us>

--- disable.sh
+++ disable.sh
@@ -32,3 +32,4 @@ rename patch patch.disabled CVE-2019-14070/3.4/0008.patch;
 rename patch patch.disabled 0006-AndroidHardening-Kernel_Hardening/3.18/0045.patch; #breaks some programs
 rename patch patch.disabled CVE-2021-39713/ANY/0011.patch; #requires CONFIG_MODULES
 rename patch patch.disabled CVE-2022-1462/*/*.patch; #requires tty_flip_buffer_commit backport
+rename patch patch.disabled CVE-2022-3640/*/*.patch; #requires torvalds/d0be8347c623
