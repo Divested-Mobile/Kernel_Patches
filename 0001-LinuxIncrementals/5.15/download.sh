ba7fba372ee949d49abe0304f57a3e71798d8bb0
Tad <tad@spotco.us>
2023-12-30 15:49:03 -0500

    Update incrementals
    Signed-off-by: Tad <tad@spotco.us>

--- 0001-LinuxIncrementals/5.15/download.sh
+++ 0001-LinuxIncrementals/5.15/download.sh
@@ -142,3 +142,4 @@ wget https://cdn.kernel.org/pub/linux/kernel/v5.x/incr/patch-5.15.140-141.xz -O 
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/incr/patch-5.15.141-142.xz -O - | xz -d > 5.15.0141-0142.patch
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/incr/patch-5.15.142-143.xz -O - | xz -d > 5.15.0142-0143.patch
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/incr/patch-5.15.143-144.xz -O - | xz -d > 5.15.0143-0144.patch
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/incr/patch-5.15.144-145.xz -O - | xz -d > 5.15.0144-0145.patch
